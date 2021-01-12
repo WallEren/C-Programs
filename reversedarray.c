f9b926c8d110fa117da4ebc8b6020f51fcd32ae5
WallEren <77011722+WallEren@users.noreply.github.com>
2021-01-12 14:57:01 +0530

    Update reversedarray.c

--- reversedarray.c
+++ reversedarray.c
@@ -4,29 +4,28 @@ int main()
 {
 	int array[100], n, i, j, temp;
   	printf("Enter number of elements in array : ");
-    scanf("%d", &n);
+    	scanf("%d", &n);
  	printf("Enter %d integers\n", n);
 
-    for (i = 0; i < n; i++)
-    {
-    	scanf("%d", &array[i]);
+    	for (i = 0; i < n; i++)
+   	{
+    		scanf("%d", &array[i]);
 	}
   	i=0;
   	j=n-1;
   	while(i<j)
-    {
-      	temp=array[i];
-      	array[i]=array[j];
-      	array[j]=temp;
-      	i++;
-      	j--;
-    }
+    	{
+      		temp=array[i];
+      		array[i]=array[j];
+      		array[j]=temp;
+      		i++;
+      		j--;
+    	}
   
-    printf("The Reverse Array \n");
+    	printf("The Reverse Array \n");
     
-    for (i = 0; i<n; i++)
-    {
-      	printf("%d", array[i]);
+    	for (i = 0; i<n; i++)
+    	{
+      		printf("%d", array[i]);
   	}
-    
 }
