885bef4e887db63a2fc74ce566e4ffb9b8f8716b
WallEren <77011722+WallEren@users.noreply.github.com>
2021-01-12 14:47:40 +0530

    Create reversedarray.c

---
+++ reversedarray.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+	int array[100], n, i, j, temp;
+  	printf("Enter number of elements in array : ");
+    scanf("%d", &n);
+ 	printf("Enter %d integers\n", n);
+
+    for (i = 0; i < n; i++)
+    {
+    	scanf("%d", &array[i]);
+	}
+  	i=0;
+  	j=n-1;
+  	while(i<j)
+    {
+      	temp=array[i];
+      	array[i]=array[j];
+      	array[j]=temp;
+      	i++;
+      	j--;
+    }
+  
+    printf("The Reverse Array \n");
+    
+    for (i = 0; i<n; i++)
+    {
+      	printf("%d", array[i]);
+  	}
+    
+}
